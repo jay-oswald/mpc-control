98bbb1dbf2694327db76beaa7b330ca2e1723eb8
jamessandison <43879953+jamessandison@users.noreply.github.com>
2018-10-05 21:33:29 +1000

    adding row 2

--- src/pgControls.vue
+++ src/pgControls.vue
@@ -3,6 +3,10 @@
 	<div class="col-md-2 fader-container">
 		<cue-fader :cue="[1100,1105,1110,1115,1120,1125,1130,1135,1140,1145,1150,1155,1160,1165,1170,1175,1180,1185,1190,1195,1199]" v-bind:activeQueList="$root.activeCuelists" name="Row 1" type="House Lights"></cue-fader>
 	</div>
+	<div class="col-md-2 fader-container">
+		<cue-fader :cue="[1200,1205,1210,1215,1220,1225,1230,1235,1240,1245,1250,1255,1260,1265,1270,1275,1280,1285,1290,1295,1299]" v-bind:activeQueList="$root.activeCuelists" name="Row 2" type="House Lights"></cue-fader>
+	</div>
+
 	<div class="col-md-4">
 		<cue-button :cue="12" name="Pre/Post Service" type="Presets" color="bg-green" icon="fa-star"></cue-button>
 		<cue-button :cue="13" name="Worship" type="Presets" color="bg-green" icon="fa-star"></cue-button>
