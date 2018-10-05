a07fa6261c25a131d7558f7a75c9acae77b43558
jamessandison <43879953+jamessandison@users.noreply.github.com>
2018-10-05 21:58:41 +1000

    add foh wash main

--- src/pgControls.vue
+++ src/pgControls.vue
@@ -12,6 +12,12 @@
 	<div class="col-md-2 fader-container">
 		<cue-fader :cue="[1400,1405,1410,1415,1420,1425,1430,1435,1440,1445,1450,1455,1460,1465,1470,1475,1480,1485,1490,1495,1499]" v-bind:activeQueList="$root.activeCuelists" name="Row 4" type="House Lights"></cue-fader>
 	</div>
+	<div class="col-md-2 fader-container">
+		<cue-fader :cue="[1500,1505,1510,1515,1520,1525,1530,1535,1540,1545,1550,1555,1560,1565,1570,1575,1580,1585,1590,1595,1599]" v-bind:activeQueList="$root.activeCuelists" name="Foyer" type="Foyer Lights"></cue-fader>
+	</div>
+	<div class="col-md-2 fader-container">
+		<cue-fader :cue="[2100,2105,2110,2115,2120,2125,2130,2135,2140,2145,2150,2155,2160,2165,2170,2175,2180,2185,2190,2195,2199]" v-bind:activeQueList="$root.activeCuelists" name="Main Wash" type="FOH Lights"></cue-fader>
+	</div>
 
 	<div class="col-md-4">
 		<cue-button :cue="12" name="Pre/Post Service" type="Presets" color="bg-green" icon="fa-star"></cue-button>
